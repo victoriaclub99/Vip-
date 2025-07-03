ca19cf120f807625110355eafeb2f9161aea538c
victorclub <218116398+victoriaclub99@users.noreply.github.com>
2025-07-03 14:19:01 +0700

    Update MainActivity.java
    Signed-off-by: victorclub <218116398+victoriaclub99@users.noreply.github.com>

--- android/app/src/main/java/MainActivity.java
+++ android/app/src/main/java/MainActivity.java
@@ -1,7 +1,5 @@
-package com.example.vip; // เปลี่ยนให้ตรงกับ applicationId ของพี่ใน build.gradle(.kts)
-
+package com.example.vip;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-  // ไม่ต้อง override onCreate หรือ register plugin อีกแล้ว!
 }
